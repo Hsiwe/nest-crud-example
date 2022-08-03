13bfcd909495472049816a62408edf856eb16e01
Evgeniy <esorokinof@gmail.com>
2022-08-03 12:24:51 +0300

    initial commit

---
+++ prisma/migrations/20220803091445_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "nickname" VARCHAR(30) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" SERIAL NOT NULL,
+    "creator" TEXT NOT NULL,
+    "name" VARCHAR(40) NOT NULL,
+    "sordOrder" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Tag" ADD CONSTRAINT "Tag_creator_fkey" FOREIGN KEY ("creator") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
